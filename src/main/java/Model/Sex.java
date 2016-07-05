ad60134dd727281545305f735c99c4ebd4ee15bb
psyclr <psyclr@live.ru>
2016-07-05 14:33:49 +0300

    commit

---
+++ src/main/java/Model/Sex.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**
+ * Created by Alex on 05.07.16.
+ */
+
+public enum Sex {
+    MALE, FEMALE
+}
---
+++ src/main/java/Model/Wall.java
@@ -0,0 +1,11 @@
+package Model;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 05.07.16.
+ */
+public class Wall {
+
+    private List<Post> wall;
+}
